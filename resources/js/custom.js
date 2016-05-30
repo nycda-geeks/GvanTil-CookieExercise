becbbaeebae81f93a4deeaaa4dde476e0a952405
Gijs van Til <gijsvantil@gmail.com>
2016-05-30 11:40:39 +0200

    created two ways to set a cookie. A simple and a complicated one

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,24 @@
-Cookies.set('visited', true);
+// if (Cookies.get('visited') == undefined) {
+// Cookies.set('visited', 'false');
+// }
+// console.log (Cookies.get('visited'))
 
-console.log(Cookies.get('visited'))
+// if (Cookies.get('visited') == 'false'){
+// 	window.alert("Welcome")
+// } else {
+// 	window.alert("Welcome back!!")
+// }
+
+// if (Cookies.get('visited') == 'false'){
+// 	Cookies.set('visited', 'true')
+// }
+
+// console.log(Cookies.get('visited'))
+
+if(Cookies.get('something') !== undefined) {
+      alert("Welcome back!!");
+    } else {
+      alert("Welcome!")
+    }
+
+Cookies.set('something', { stuff: 'more stuff' });
